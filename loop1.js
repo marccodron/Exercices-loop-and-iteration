b81d21ff6e96fd16649f91d4784bf8bd64c58522
marccodron <logs@marc-codron.fr>
2021-02-28 23:05:10 +0100

    add def

---
+++ loop1.js
@@ -0,0 +1,11 @@
+//Avec une boucle while afficher les éléments du tableau suivant, ligne par ligne:
+
+
+let tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let i = 0
+
+while (i < tab.length){
+  console.log(tab[i])
+  i++
+}
