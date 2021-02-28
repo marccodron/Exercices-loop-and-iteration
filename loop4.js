a1d8e05cc5801c17d82e0228dd6823b1504e0fc9
marccodron <logs@marc-codron.fr>
2021-02-28 23:24:36 +0100

    add def

---
+++ loop4.js
@@ -0,0 +1,7 @@
+//Avec une boucle for-of afficher les éléments du tableau suivant, ligne par ligne:
+
+let tab = ['Js', 'Solidity', 'Html', 'Css', 'Python', 'Java', 'C++', 'Rust']
+
+for(let string of tab) {
+  console.log(string)
+}
