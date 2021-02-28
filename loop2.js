09b1d7f67f28d4b70ce688cf8811a4884766d939
marccodron <logs@marc-codron.fr>
2021-02-28 23:17:19 +0100

    add def

---
+++ loop2.js
@@ -0,0 +1,11 @@
+//Avec une boucle do-while afficher les éléments du tableau suivant, ligne par ligne:
+
+let tab = ['a', 'b', 'c', 'd', 'e']
+console.log(tab) // to be changed
+
+let i = 0 // index 0
+
+do {
+  console.log(tab[i]) 
+  i++  // increment assign
+} while (i < tab.length) // until length
